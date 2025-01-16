259892c33b60b38b63ca87965adc0c260bb8faaf
red942 <cam47@mit.edu>
2025-01-15 20:01:59 -0500

    init

---
+++ client/src/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import Skeleton from "./components/pages/Skeleton";
+import NotFound from "./components/pages/NotFound";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "1046410330477-drrhfr4ahdebua2e4deskpaoo3n17on4.apps.googleusercontent.com";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<NotFound />} element={<App />}>
+      <Route path="/" element={<Skeleton />}/>
+    </Route>
+  )
+)
+
+// renders React Component "Root" into the DOM element with ID "root"
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <RouterProvider router={router} />
+  </GoogleOAuthProvider>
+);
