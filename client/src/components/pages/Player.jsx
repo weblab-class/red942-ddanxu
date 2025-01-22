5448f502e9467905f6e9f111d4ba428079cf762a
red47 <cam47@mit.edu>
2025-01-22 17:47:25 -0500

    added basic css

--- client/src/components/pages/Player.jsx
+++ client/src/components/pages/Player.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { get, post } from "../../utilities";
-import "./Editor.css";
+import "./Player.css";
 
 const Player = () => {
   const navigate = useNavigate();
@@ -11,32 +11,33 @@ const Player = () => {
   const [user, setUser] = useState();
 
   useEffect(() => {
-
-    let frameId = null
+    let frameId = null;
 
     get("/api/novel", { novelId: location.state.novelId }).then((res) => {
       Document.title = res.novel.name;
       setNovel(res.novel);
       if (typeof location.state.frameId != "undefined") {
-        frameId = location.state.frameId
+        frameId = location.state.frameId;
         get("/api/frame", { frameId: location.state.frameId }).then((res2) => setFrame(res2.frame));
       } else {
-        frameId = location.state.frameId
+        frameId = location.state.frameId;
         get("/api/frame", { frameId: res.novel.startFrameId }).then((res2) => setFrame(res2.frame));
       }
     });
 
-    
     get(`/api/user`, { userid: location.state.userId }).then((userObj) => {
-        const hasPlayed = userObj.playing.some(item => item.novelId === location.state.novelId);
+      const hasPlayed = userObj.playing.some((item) => item.novelId === location.state.novelId);
 
-        if (!hasPlayed) {
-            post('/api/userPlayNew', {userId:location.state.userId, novelId:location.state.novelId, frameId: frameId}).then();
-        }
+      if (!hasPlayed) {
+        post("/api/userPlayNew", {
+          userId: location.state.userId,
+          novelId: location.state.novelId,
+          frameId: frameId,
+        }).then();
+      }
 
-        setUser(userObj)
+      setUser(userObj);
     });
-
   }, [location.state]);
 
   const refresh = () => {
@@ -47,36 +48,31 @@ const Player = () => {
 
   const nextFrame = async () => {
     if (frame.nextFrame != undefined) {
-
       refresh();
 
       navigate("/player/" + frame.nextFrame, {
         state: {
           novelId: novel._id,
           frameId: frame.nextFrame,
-        }, replace: true
+        },
+        replace: true,
       });
-    } else 
-      console.log("End of Novel!");
+    } else console.log("End of Novel!");
   };
-
-
 
   if (!frame) {
     return <h2>loading...</h2>;
   }
 
   return (
-    <>
+    <div className="visual-novel" style={{ backgroundImage: `url(${frame.background})` }}>
       <h1>Playing {novel.name}</h1>
-      <h3>Currently looking at frame with id {frame._id}</h3>
-      <img src={frame.background}/>
-      <img src={frame.spriteLeft}/>
-      <img src={frame.spriteMid}/>
-      <img src={frame.spriteRight}/>
+      {frame.spriteLeft && <img src={frame.spriteLeft} className="sprite-left" alt="sprite"/>}
+      {frame.spriteMid && <img src={frame.spriteMid} className="sprite-mid" alt="sprite"/>}
+      {frame.spriteRight && <img src={frame.spriteRight} className="sprite-right" alt="sprite"/>}
       <h3>{frame.text}</h3>
       <button onClick={nextFrame}>next</button>
-    </>
+    </div>
   );
 };
 
